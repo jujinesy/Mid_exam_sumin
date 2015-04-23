7b6604f87f2432a871e03731ad06071880e8b7db
Ju Jin BAE <jujinesy@gmail.com>
2015-04-23 14:12:10 +0900

    주석달음;

--- Mid_exam_sumin/prob1.c
+++ Mid_exam_sumin/prob1.c
@@ -10,14 +10,16 @@ void trial(int input, int *result)
 	int getout, b, isprime;
 	for (getout = 0; getout == 0; getout++)
 	{
-		if (input % 2 == 0 && input != 2)
+		if (input % 2 == 0 && input != 2) //소수를 찾기 위해서 사용됨. 2를 나눠서 
 		{
-			*result = 0;
+			/////////////////////////////////////////
+			*result = 0; //값이 0이될경우는 소수가 아니라고 인식
 			printf("%d는 소수 아님\n", input);
+			/////////////////////////////////////////
 			continue;
 		}
 		isprime = 1;
-		for (b = 2; b*b <= input; b++)
+		for (b = 2; b*b <= input; b++) //루트개념: 소수 찾을 때 사용됨 (교수님 찾앗어요. 안고칠꺼에요. 저도 뭔지 몰라요.)
 		{
 			if (input%b == 0)
 			{
@@ -27,32 +29,35 @@ void trial(int input, int *result)
 		}
 		if (isprime)
 		{
-			*result = 1;
+			*result = 1; //값이 1이 될 경우는 소수로 인식한다.
 			printf("%d는 소수\n", input);
-		}else
+		}
+		else
 		{
 			*result = 0;
 			printf("%d는 소수 아님\n", input);
 		}
-		
+
 	}
 }
 
-int main()
+int main() //시작
 {
-	int result;
-	int low, high, i;
+	int result; //소수일때 값이 1, 아닐때는 0
+	int low, high, i; //사용자가 입력한 낮은 값이 low, 높은 값이 high
 
-	printf("두 숫자를 입력하세요. 예)12 67\n");
-	scanf("%d %d", &low, &high);
+	printf("두 숫자를 입력하세요. 예)12 67\n"); //검은화면 뜨는 거
+	scanf("%d %d", &low, &high); //입력하는거
 
 
-	for (i = high; i >= low; i--)
+	for (i = high; i >= low; i--) //i의 변수 
+		//for (i = low; i >= high; i++) //i의 변수 prime 소수가 가장 작을때
 	{
-		trial(i, &result);
+		trial(i, &result); //i(high값)이 low값이 될 때 까지 빼기
 		if (result == 1)
 			break;
 	}
-	
- 	return 0;
+	getchar(); //팍 안꺼지게
+
+	return 0; //종료
 }
--- Mid_exam_sumin/prob3.c
+++ Mid_exam_sumin/prob3.c
@@ -9,31 +9,32 @@ int gcd(int a, int b);
 
 int main(void) {
 	int count, num1, num2, num3, num4, num5;
-	
+
 	while (1)
 	{
-		printf("최대공약수 구할 갯수 입력(2~5)\n");
+		printf("최대공약수 구할 갯수 입력(2~5)\n"); //최대 공약수 숫자 n개 숫자 입력
 		scanf("%d", &count);
 
-		if (count == 2)
+		if (count == 2) //2를 입력했을때
 		{
 			printf("숫자를 입력하세요. 예) 1 2\n");
 			scanf("%d %d", &num1, &num2);
-			printf("%d\n", gcd(num1, num2));
+			printf("%d\n", gcd(num1, num2)); //gce가 최대 공약수
+			//printf("%d\n", gcd(num1, num2)*num1*num2);
 		}
-		else if (count == 3)
+		else if (count == 3) //3
 		{
 			printf("숫자를 입력하세요. 예) 1 2 3\n");
 			scanf("%d %d %d", &num1, &num2, &num3);
 			printf("%d\n", gcd(gcd(num1, num2), num3));
 		}
-		else if (count == 4)
+		else if (count == 4) //4
 		{
 			printf("숫자를 입력하세요. 예) 1 2 3 4\n");
 			scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
 			printf("%d\n", gcd(gcd(gcd(num1, num2), num3), num4));
 		}
-		else if (count == 5)
+		else if (count == 5) //5
 		{
 			printf("숫자를 입력하세요. 예) 1 2 3 4 5\n");
 			scanf("%d %d %d %d %d", &num1, &num2, &num3, &num4, &num5);
